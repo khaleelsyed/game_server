cb5f519461d7f0deb253944b96ef14e0a8a90d27
khaleelsyed <khaleelsyed@users.noreply.github.com>
2025-01-09 23:41:04 +0000

    feat: update client script to first pass on conn refused

--- client/main.go
+++ client/main.go
@@ -1,3 +1,54 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"math"
+
+	"github.com/gorilla/websocket"
+	"golang.org/x/exp/rand"
+)
+
+const wsServerEndpoint = "ws://localhost:40000"
+
+type LoginData struct {
+	ClientID int    `json:"client_id"`
+	Username string `json:"username"`
+}
+
+type GameClient struct {
+	conn     *websocket.Conn
+	clientID int
+	Username string
+}
+
+func (c *GameClient) login() error {
+	return c.conn.WriteJSON(LoginData{
+		ClientID: c.clientID,
+		Username: c.Username,
+	})
+}
+
+func newGameClient(conn *websocket.Conn, username string) *GameClient {
+	return &GameClient{
+		conn:     conn,
+		clientID: rand.Intn(math.MaxInt),
+		Username: username,
+	}
+}
+
+func main() {
+	dialer := websocket.Dialer{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
+	conn, _, err := dialer.Dial(wsServerEndpoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := newGameClient(conn, "James")
+	if err := c.login(); err != nil {
+		log.Fatal(err)
+	}
+}
